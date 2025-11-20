7d0dd5e044bfe9e525531eeba13fc0a98a0ac9cd
Gustavo Mamede <gustavo@vestelivre.com.br>
2025-11-20 17:01:10 -0300

    adicionar contador regressivo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ NOME_ANIVERSARIANTE = "Ana Luíza"
 DATA_FESTA = "2025-12-06 18:00:00"  # AAAA-MM-DD HH:MM:SS
 LOCAL_NOME = "Condominio Morada do Sol"
 LOCAL_ENDERECO = "Rua Tamarindos, 600"
-LINK_MAPS = "https://www.google.com/maps?q=Buffet+Espaco+Festas"
+LINK_MAPS = "https://www.google.com/maps/place/R.+Tamarindos,+600+-+Morada+do+Sol,+Uberl%C3%A2ndia+-+MG,+38415-474/@-18.8952451,-48.3473766,206m/data=!3m1!1e3!4m6!3m5!1s0x94a44164977b417d:0x39f7cd92e11fd7e1!8m2!3d-18.8954859!4d-48.3474985!16s%2Fg%2F11lcc6s55r!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MTExNy4wIKXMDSoASAFQAw%3D%3D"
 
 # --------- ROTAS ---------
 @app.route("/")
@@ -26,6 +26,7 @@ def index():
         link_maps=LINK_MAPS,
         data_formatada=data_formatada,
         hora_formatada=hora_formatada,
+        data_festa_iso=DATA_FESTA,
     )
 
 
